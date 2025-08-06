7d51b43da4153449a57c12b9e801cba5061ad0a0
Mohammad Jannatul Baki <jannatul14.baki@gmail.com>
2025-08-06 23:52:17 +0600

    init

---
+++ ServiceA/Controllers/MessageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceA.Controllers;
+
+[ApiController]
+[Route("Message")]
+public class MessageController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello from ServiceA!");
+    }
+}
---
+++ ServiceB/Controllers/CallServiceAController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceB.Controllers;
+
+[ApiController]
+[Route("CallServiceA")]
+public class CallServiceAController(IHttpClientFactory httpClientFactory) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var httpClient = httpClientFactory.CreateClient("ServiceAClient");
+
+        try
+        {
+            var response = await httpClient.GetStringAsync("https://localhost:7283/Message");
+            return Ok($"Hello from ServiceB! ServiceA says: {response}");
+        }
+        catch (HttpRequestException ex)
+        {
+            return StatusCode(500, $"Error calling ServiceA: {ex.Message}");
+        }
+    }
+}
